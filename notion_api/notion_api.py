43983ab55acebebdafd42410f615637fca0c5833
ik-y <yuu@imago.mobi>
2023-06-11 23:27:29 +0900

    Created notion object initializer

--- notion_api/notion_api.py
+++ notion_api/notion_api.py
@@ -1,2 +1,10 @@
-def test():
-    print("hello world")
+class Notion:
+  def __init__(self, api_key) -> None:
+    self.api_key = api_key
+    self.headers = {
+      "Accept": "application/json",
+      "Notion-Version": "2022-06-28",
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + api_key
+    }
+    self.api = "https://api.notion.com/v1/"
