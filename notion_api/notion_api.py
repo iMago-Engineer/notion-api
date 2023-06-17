f2a09b885be0e993e84307464b2fcde5510da6b8
ik-y <yuu@imago.mobi>
2023-06-17 18:12:42 +0900

    db apiの実装2

--- notion_api/notion_api.py
+++ notion_api/notion_api.py
@@ -1,3 +1,5 @@
+import requests
+
 class Notion:
   def __init__(self, api_key) -> None:
     self.api_key = api_key
@@ -8,3 +10,18 @@ class Notion:
       "Authorization": "Bearer " + api_key
     }
     self.api = "https://api.notion.com/v1/"
+
+  def get_one_page(self, page_id):
+    url = "{}pages/{}".format(self.api, page_id)
+    response = requests.get(url, headers=self.headers)
+    return response.json()
+
+  def get_database(self, database_id):
+    url = "{}databases/{}".format(self.api, database_id)
+    response = requests.get(url, headers=self.headers)
+    return response.json()
+
+  def query_database(self, database_id):
+    url = "{}databases/{}/query".format(self.api, database_id)
+    response = requests.post(url, headers=self.headers)
+    return response.json()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="notio_-api",
+    name="notion-api",
     version="0.0.1",
     author="ik-y",
     packages=find_packages(),
