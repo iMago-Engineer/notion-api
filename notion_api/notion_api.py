112d4aad9932adc7ce13cf2ba09e5ad6efadbed8
ik-y <yuu@imago.mobi>
2023-06-23 23:28:54 +0900

    キャッシュ機構を追加した(キャッシュリセットは.notion.cache ファイルの削除で可能

--- notion_api/notion_api.py
+++ notion_api/notion_api.py
@@ -1,4 +1,6 @@
 import requests
+import glob
+import json
 
 class Notion:
   def __init__(self, api_key) -> None:
@@ -10,15 +12,39 @@ class Notion:
       "Authorization": "Bearer " + api_key
     }
     self.api = "https://api.notion.com/v1/"
+    if len(glob.glob(".notion.cache")) != 0:
+      try:
+        self.cache = json.loads(open(".notion.cache", "r").read())
+      except:
+        print("Error: .notion.cache is corrupted. Please delete it and try again.")
+        self.initialize_cache()
+    else:
+      self.initialize_cache()
+
+  def initialize_cache(self):
+    self.cache = dict()
+    self.cache["pages"] = dict()
+    self.cache["databases"] = dict()
+
+  def save_cache(self):
+    open(".notion.cache", "w").write(json.dumps(self.cache))
 
   def get_one_page(self, page_id):
+    if page_id in self.cache["pages"].keys():
+      return self.cache["pages"][page_id]
     url = "{}pages/{}".format(self.api, page_id)
     response = requests.get(url, headers=self.headers)
+    self.cache["pages"][page_id] = response.json()
+    self.save_cache()
     return response.json()
 
   def get_database(self, database_id):
+    if page_id in self.cache["databases"].keys():
+      return self.cache["databases"][database_id]
     url = "{}databases/{}".format(self.api, database_id)
     response = requests.get(url, headers=self.headers)
+    self.cache["databases"][database_id] = response.json()
+    self.save_cache()
     return response.json()
 
   def query_database(self, database_id):
